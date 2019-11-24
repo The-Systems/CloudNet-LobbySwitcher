aee1aa983e6633018bc632296e95134fda9921b6
derrop <roclaromi@gmail.com>
2019-11-24 20:01:50 +0100

    Fix that the menu opened when walking over a pressure plate

--- Core/src/main/java/eu/thesystems/cloudnet/lobbyswitcher/core/listener/SwitchListener.java
+++ Core/src/main/java/eu/thesystems/cloudnet/lobbyswitcher/core/listener/SwitchListener.java
@@ -11,6 +11,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -50,12 +51,14 @@ public class SwitchListener implements Listener {
 
     @EventHandler
     public void handleInteract(PlayerInteractEvent event) {
-        GroupConfig groupConfig = CloudNetLobbySwitcher.getInstance().getSelfGroupConfig();
-        if (groupConfig != null) {
-            Player player = event.getPlayer();
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) {
+            GroupConfig groupConfig = CloudNetLobbySwitcher.getInstance().getSelfGroupConfig();
+            if (groupConfig != null) {
+                Player player = event.getPlayer();
 
-            if (groupConfig.handleInteract(player, player.getInventory().getHeldItemSlot())) {
-                event.setCancelled(true);
+                if (groupConfig.handleInteract(player, player.getInventory().getHeldItemSlot())) {
+                    event.setCancelled(true);
+                }
             }
         }
     }
