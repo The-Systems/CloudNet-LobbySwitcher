4d94bdc9ef7c987eebc158fbcb4fcfab88d583cc
derrop <derrop@outlook.de>
2020-04-22 00:59:28 +0200

    Fix that servers don't get removed after stopping them in CloudNet 3

--- CloudNet3/src/main/java/eu/thesystems/cloudnet/lobbyswitcher/v3/CloudNet3ServersListener.java
+++ CloudNet3/src/main/java/eu/thesystems/cloudnet/lobbyswitcher/v3/CloudNet3ServersListener.java
@@ -5,6 +5,7 @@ package eu.thesystems.cloudnet.lobbyswitcher.v3;
 
 import de.dytanic.cloudnet.driver.event.EventListener;
 import de.dytanic.cloudnet.driver.event.events.service.CloudServiceInfoUpdateEvent;
+import de.dytanic.cloudnet.driver.event.events.service.CloudServiceStopEvent;
 import eu.thesystems.cloudnet.lobbyswitcher.core.servers.SimplifiedServerInfo;
 import lombok.AllArgsConstructor;
 
@@ -20,6 +21,14 @@ public class CloudNet3ServersListener {
         SimplifiedServerInfo serverInfo = this.switcher.createServerInfo(event.getServiceInfo());
         if (serverInfo != null) {
             this.switcher.getSwitcher().handleServerInfoUpdate(null, serverInfo);
+        }
+    }
+
+    @EventListener
+    public void handleServerStop(CloudServiceStopEvent event) {
+        SimplifiedServerInfo serverInfo = this.switcher.createServerInfo(event.getServiceInfo());
+        if (serverInfo != null) {
+            this.switcher.getSwitcher().handleServerRemove(serverInfo);
         }
     }
 
